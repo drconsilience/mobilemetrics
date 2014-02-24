36b68aeccdc5356929bb1612ada6091b59debcf7
pete8236 <pete8236@umn.edu>
2014-02-24 13:21:58 -0600

    trying this

--- MainActivity/src/com/mobilemetric/main/DataChoose.java
+++ MainActivity/src/com/mobilemetric/main/DataChoose.java
@@ -5,7 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+//Test Comment here
 public class DataChoose extends Activity{
 
 

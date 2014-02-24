1d0e05dedda6bc16acf09dfd7ca3cfa644d40d6c
talkingclock10 <sperr045@umn.edu>
2014-02-24 13:57:55 -0600

    10Hz data collection

--- MobileMetric/src/ti/android/ble/sensortag/DeviceActivity.java
+++ MobileMetric/src/ti/android/ble/sensortag/DeviceActivity.java
@@ -340,6 +340,15 @@ public class DeviceActivity extends ViewPagerActivity {
 			}
 			
   		BluetoothGattService serv = mBtGatt.getService(servUuid);
+  		
+  	// Accelerometer calibration
+        if (confUuid.equals(SensorTag.UUID_ACC_CONF) && enable) {
+            byte transmitPeriod = 10; // in tens of milliseconds
+            BluetoothGattCharacteristic characPeriod = serv.getCharacteristic(SensorTag.UUID_ACC_PERI);
+            mBtLeService.writeCharacteristic(characPeriod, transmitPeriod);
+            mBtLeService.waitIdle(GATT_TIMEOUT);
+        }
+  		
   		BluetoothGattCharacteristic charac = serv.getCharacteristic(confUuid);
   		byte value =  enable ? sensor.getEnableSensorCode() : TagSensor.DISABLE_SENSOR_CODE;
   		mBtLeService.writeCharacteristic(charac, value);
